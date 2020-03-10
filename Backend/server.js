920797495e111a0cefb5562b4ed7aab2e1671e42
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-10 00:50:28 -0600

    feat: Conexion DB.

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 'use strict';
-
+require('dotenv').config();
 var app = require('./index');
 var http = require('http');
 var cors = require('cors');
 var express = require('express');
 var morgan = require('morgan');
+var { Sequelize } = require('data-layer');
 
 var server;
 
@@ -16,13 +17,17 @@ server = http.createServer(app);
 server.listen(process.env.PORT || 8000);
 
 //midlewares 
-app.use(cors({origin:'http://localhost:4200'}));    // direccion del frontend
+app.use(cors({ origin: 'http://localhost:4200' }));    // direccion del frontend
 app.use(express.json());
 app.use(morgan('dev'));
 
 
-server.on('listening', function () {
-    console.log('Server listening on http://localhost:%d', this.address().port);
+var DB = new Sequelize(process.env.DB_HOST, process.env.DB_PORT, process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
+
+DB.conectarDB().then(function () {
+    server.on('listening', function () {
+        console.log('Server listening on http://localhost:%d', this.address().port);
+    });
 });
 
 
