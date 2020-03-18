ff4706221c94bcf976021e72d91addd44ff49321
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-18 15:06:27 -0600

    feat: Modulo data-layer agregado.

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,13 @@ var cors = require('cors');
 var express = require('express');
 var morgan = require('morgan');
 var { Sequelize } = require('data-layer');
-
+let db = 10;
 var server;
-
+const DB = new Sequelize(process.env.DB_HOST
+    , process.env.DB_PORT
+    , process.env.DB_NAME
+    , process.env.DB_USER
+    , process.env.DB_PASS);
 /*
  * Create and start HTTP server.
  */
@@ -20,14 +24,17 @@ server.listen(process.env.PORT || 8000);
 app.use(cors({ origin: 'http://localhost:4200' }));    // direccion del frontend
 app.use(express.json());
 app.use(morgan('dev'));
+// database integration inside modules.
+const dbx = function (req, _, next) { req.db = db; next(); };
+app.use(dbx.bind(db));
 
-
-var DB = new Sequelize(process.env.DB_HOST, process.env.DB_PORT, process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
-
-DB.conectarDB().then(function () {
-    server.on('listening', function () {
-        console.log('Server listening on http://localhost:%d', this.address().port);
-    });
-});
+DB.conectarDB()
+    .then((_db) => {
+        db = _db;
+        server.on('listening', function () {
+            console.log('Server listening on http://localhost:%d', this.address().port);
+        });
+    })
+    .catch((e) => console.error('El sistema no se pudo conectar a la DB.', e));
 
 
