b22f2d929a7535df01c84b151cbe33ee73aaee85
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-22 03:19:25 -0600

    feat: Agregar campos a listar usuarios.

--- Backend/repository/data/UserRepository.js
+++ Backend/repository/data/UserRepository.js
@@ -1,6 +1,42 @@
 class UserRepository {
     constructor(opts) {
         this.UserDataRepository = opts.UserDataRepository;
+        this.UserTypeDataRepository = opts.UserTypeDataRepository;
+        this.UserStatusDataRepository = opts.UserStatusDataRepository;
+    }
+
+    async listaUsuarios() {
+        const response = {
+            data: null,
+            message: null,
+            success: true
+        };
+        try {
+            // Listado de usuarios ordenados alfabeticamente
+            response.data = await this.UserDataRepository.findAll({
+                raw: true,
+                attributes: {
+                    include: [
+                        [this.UserTypeDataRepository.sequelize.col('tipoUsuario.nombre'), 'nombre_tipo_usuario'],
+                        [this.UserStatusDataRepository.sequelize.col('estadoUsuario.nombre'), 'nombre_estado_usuario']
+                    ]
+                },
+                include: [{
+                    model: this.UserTypeDataRepository,
+                    required: true,
+                    attributes: []
+                }, {
+                    model: this.UserStatusDataRepository,
+                    required: true,
+                    attributes: []
+                }],
+                order: [['nombre', 'ASC'], ['apellido', 'ASC']]
+            });
+        } catch (error) {
+            response.success = false;
+            response.message = 'Error al obtener usuarios, intente mas tarde.'
+        }
+        return response;
     }
 
     async inicioSesion(body) {
@@ -82,21 +118,6 @@ class UserRepository {
         } catch (error) {
             response.success = false;
             response.message = 'Error al eliminar un usuario, intente mas tarde.'
-        }
-        return response;
-    }
-
-    async listaUsuarios() {
-        const response = {
-            data: null,
-            message: null,
-            success: true
-        };
-        try {
-            response.data = await this.UserDataRepository.findAll({ order: [['nombre', 'ASC'], ['apellido', 'ASC']] });
-        } catch (error) {
-            response.success = false;
-            response.message = 'Error al obtener usuarios, intente mas tarde.'
         }
         return response;
     }
