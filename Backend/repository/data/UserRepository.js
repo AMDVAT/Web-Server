ab4ad40668a064456d0765d980fdfb7f430300b1
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-22 00:56:11 -0600

    feat: Parametros directos dentro del metodo.

--- Backend/repository/data/UserRepository.js
+++ Backend/repository/data/UserRepository.js
@@ -3,16 +3,16 @@ class UserRepository {
         this.UserDataRepository = opts.UserDataRepository;
     }
 
-    async inicioSesion(email, contrasena) {
+    async inicioSesion(body) {
+        const { email, contrasena } = body;
         const response = {
             data: null,
             message: null,
             success: true
         };
         try {
-            const data = await this.UserDataRepository
+            response.data = await this.UserDataRepository
                 .findOne({ where: { email: email || null, password: contrasena || null } });
-            response.data = data.dataValues;
         } catch (error) {
             response.success = false;
             response.message = 'Error al realizar la autenticacion, intente mas tarde.'
