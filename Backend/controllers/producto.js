06e9baed566e0debdbe1b895f7bbbbc114bf25e5
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-29 01:22:12 -0600

    refactor: buscar producto

--- Backend/controllers/producto.js
+++ Backend/controllers/producto.js
@@ -32,21 +32,6 @@ producto.topCategorias = async (req, res) => {
     }
 };
 
-producto.buscarProducto = async (req, res) => {
-    try {
-        const data = await req.container.resolve('ProductRepository').buscarProducto(req.query);
-        const { data: productos } = data;
-        if (data.success && productos) {
-            res.send(productos);
-        }
-        else {
-            res.status(400).send({ mensaje: data.message });
-        }
-    } catch (error) {
-        res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
-    }
-};
-
 producto.recienIngreso = async (req, res) => {
 
     //listar productos de recien ingreso
