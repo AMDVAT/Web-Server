327e93e9084df88d127395d1bdebfc1a5fc5f99f
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-04-16 18:10:02 -0600

    feat: Listar reservas por usuario.

--- Backend/controllers/reserva/index.js
+++ Backend/controllers/reserva/index.js
@@ -33,4 +33,22 @@ module.exports = (router) => {
             res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
         }
     });
+
+    router.get('/', validarToken, async (req, res) => {
+        try {
+            const data = await req.container.resolve('ReservationRepository')
+                .listarReserva({ ...req.tokenData });
+            const { data: reservas } = data;
+            if (data.success && reservas) {
+                res.send(reservas);
+            }
+            else {
+                res.status(400).send({ mensaje: data.message });
+            }
+            res.message = data;
+        } catch (error) {
+            res.message = error;
+            res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
+        }
+    });
 };
