afe5044680eeb3ebfb4b2beffad8bfb11af6734c
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-05-04 00:17:56 -0600

    feat: Pruebas unitarias reserva.

--- Backend/src/mapeoObjetos/reserva/entrada/entradaCrearReserva.js
+++ Backend/src/mapeoObjetos/reserva/entrada/entradaCrearReserva.js
@@ -2,6 +2,7 @@ module.exports = (req) => {
     const consultarToken = require('../../../token/consultarToken');
     let now = new Date(); // Posible mapeo adicional
     const token = req.headers['token'];
+    if(!token) return null;
     return {
         data: {
             fecha: now,
