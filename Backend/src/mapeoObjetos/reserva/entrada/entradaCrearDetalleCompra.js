06ef8fd66288dd74c1897dae63c09eb83992b14a
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-05-05 00:33:00 -0600

    feat: Sucursula

--- Backend/src/mapeoObjetos/reserva/entrada/entradaCrearDetalleCompra.js
+++ Backend/src/mapeoObjetos/reserva/entrada/entradaCrearDetalleCompra.js
@@ -3,7 +3,7 @@ module.exports = (id_reserva, element) => {
         data: {
             cantidad: element.cantidad,
             id_compra: id_reserva,
-            id_sucursal: element.id_sucursal,
+            id_sucursal: 1,
             id_producto: element.id_producto
         }
     };
